3d3f76c42ecc1d5039d14bd5676d14a524d1e53f
Shubham Sharma <112383130+Shubham-AutomationTester@users.noreply.github.com>
2026-08-15 01:43:32 +0530

    Update server.py

--- server.py
+++ server.py
@@ -56,8 +56,8 @@ class TestHandler(BaseHTTPRequestHandler):
             <a href="/?size=8200">8200</a><br>
             <a href="/?size=10000">10000</a><br>
             <a href="/?size=70000">70000</a><br>
-            <a href="/?size=65536">65536</a><br>
-            <a href="/?size=65537">65537</a><br>
+            <a href="/?size=65500">65536</a><br>
+            <a href="/?size=65502">65537</a><br>
             <a href="/?size=13267">13267</a>
         </body>
         </html>
